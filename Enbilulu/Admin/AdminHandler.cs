cd93efc8ac15e20fe5f301fb040fe1ffc31b19a1
gilmae <avocadia@fastmail.fm>
2020-01-07 17:41:42 +1100

    Add beginnings of an admin interface

---
+++ Enbilulu/Admin/AdminHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nancy;
+namespace Enbilulu.WebServer
+{
+    public class AdminHandler : NancyModule
+    {
+        public AdminHandler()
+        {
+            Get("/admin", p =>
+            {
+                var streams =  GetDb().ListStreams();
+
+                return View["WebServer/index.html", streams];
+            });
+        }
+
+        private Db GetDb()
+        {
+            return new Db(Environment.GetEnvironmentVariable("DataFolder"));
+        }
+    }
+}
