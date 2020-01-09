caad9c25c6a84aa5db4222c3e6671cc954bb78d6
gilmae <avocadia@fastmail.fm>
2020-01-09 12:01:15 +1100

    Enable CSRF protection

--- Enbilulu/API/Bootstrapper.cs
+++ Enbilulu/API/Bootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
 
 namespace Enbilulu
@@ -9,6 +10,12 @@ namespace Enbilulu
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
             base.ConfigureApplicationContainer(container);
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Nancy.Security.Csrf.Enable(pipelines);
         }
     }
 }
