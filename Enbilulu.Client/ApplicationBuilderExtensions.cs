84fd382a8f568befd19280c6bbd94b793dfc7aa5
gilmae <avocadia@fastmail.fm>
2022-07-04 17:53:54 +1000

    Adds extensions for DI

---
+++ Enbilulu.Client/ApplicationBuilderExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+public class EnbiluluBuilder
+{
+    Enbilulu.Client _client;
+    IConfiguration _config;
+    public EnbiluluBuilder(Enbilulu.Client client, IConfiguration config)
+    {
+        _client = client;
+        _config = config;
+    }
+
+    public EnbiluluBuilder UseConnectionString(string connectionString)
+    {
+        _client.Endpoint = new Uri(connectionString);
+        return this;
+    }
+
+    public EnbiluluBuilder UseConnectionStringName(string connectionStringName)
+    {
+        return UseConnectionString(_config.GetConnectionString(connectionStringName));
+    }
+
+    public EnbiluluBuilder UseStream(string streamName)
+    {
+        _client.CreateStream(streamName);
+        return this;
+    }
+}
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseEnbilulu(this IApplicationBuilder builder, Action<EnbiluluBuilder> configure)
+    {
+
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        if (configure == null)
+        {
+            throw new ArgumentNullException(nameof(configure));
+        }
+
+        EnbiluluBuilder enbiluluBuilder = builder.ApplicationServices.GetService(typeof(EnbiluluBuilder)) as EnbiluluBuilder;
+
+        if (enbiluluBuilder == null)
+        {
+            throw new ArgumentNullException(nameof(enbiluluBuilder));
+        }
+
+        configure(enbiluluBuilder);
+
+        return builder;
+
+    }
+}
--- Enbilulu.Client/ServicesExtensions.cs
+++ Enbilulu.Client/ServicesExtensions.cs
@@ -1,34 +1,18 @@
-
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Enbilulu;
-public static class ServicesExtensions {
-    public static void AddEnbilulu (this IServiceCollection services) {
-        services.AddEnbilulu(new EnbiluluSettings());
-    }
-
-    public static void AddEnbilulu(this IServiceCollection services, EnbiluluSettings settings)
+public static class ServicesExtensions
+{
+    public static void AddEnbilulu(this IServiceCollection services)
     {
-        Client enbilulu = new Client(settings.ConnectionString);
-        services.AddSingleton(typeof(Client), enbilulu);
+        Enbilulu.Client enbilulu = new Enbilulu.Client();
+        services.AddSingleton(typeof(Enbilulu.Client), enbilulu);
         services.AddTransient(typeof(EnbiluluBuilder));
     }
 }
 
-public class EnbiluluSettings
-{
-    public string ConnectionString { get; set; } = default!;
-}
 
-public class EnbiluluBuilder {
-    Client _client;
-    public EnbiluluBuilder (Client client) {
-        _client = client;
-    }
-    
-    public EnbiluluBuilder UseStream (string streamName) {
-        _client.CreateStream (streamName);
-        return this;
-    }
-}
 
