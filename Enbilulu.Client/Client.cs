c553f0ffd95a855e85d19d04a13f9e00272b5f3f
gilmae <avocadia@fastmail.fm>
2022-08-20 17:45:11 +1000

    Updates RestSharp

--- Enbilulu.Client/Client.cs
+++ Enbilulu.Client/Client.cs
@@ -24,66 +24,63 @@ public class Client
     {
         var client = new RestClient(Endpoint);
 
-        var request = new RestRequest("/streams/{stream}", Method.POST);
+        var request = new RestRequest("/streams/{stream}", Method.Post);
         request.AddUrlSegment("stream", stream);
 
         var response = client.Post<Stream>(request);
 
-        return response.Data;
+        return response;
     }
 
     public Stream PutRecord(string stream, dynamic data)
     {
         var client = new RestClient(Endpoint);
 
-        var request = new RestRequest("/streams/{stream}/point", Method.POST);
+        var request = new RestRequest("/streams/{stream}/point", Method.Post);
         request.AddUrlSegment("stream", stream);
-        request.AddJsonBody(data);
+        request.AddJsonBody<object>(data as object);
 
         var response = client.Post<Stream>(request);
 
-        return response.Data;
+        return response;
     }
 
     public Stream GetStream(string stream)
     {
         var client = new RestClient(Endpoint);
 
-        var request = new RestRequest("/streams/{stream}", Method.GET);
+        var request = new RestRequest("/streams/{stream}", Method.Get);
         request.AddUrlSegment("stream", stream);
 
         var response = client.Get<Stream>(request);
 
-        if (response.IsSuccessful)
-        {
-            return response.Data;
-        }
-        return null;
+        return response;
+        
     }
 
     public Section GetRecordsFrom(string stream, int start, int limit)
     {
         var client = new RestClient(Endpoint);
 
-        var request = new RestRequest("/streams/{stream}/{start}/{limit}", Method.GET);
+        var request = new RestRequest("/streams/{stream}/{start}/{limit}", Method.Get);
         request.AddUrlSegment("stream", stream);
         request.AddUrlSegment("start", start);
         request.AddUrlSegment("limit", limit);
 
         var response = client.Get<Section>(request);
 
-        return response.Data;
+        return response;
     }
 
     public IList<string> ListStreams()
     {
         var client = new RestClient(Endpoint);
 
-        var request = new RestRequest("/streams", Method.GET);
+        var request = new RestRequest("/streams", Method.Get);
 
         var response = client.Get<List<string>>(request);
 
-        return response.Data;
+        return response;
     }
 
 }
