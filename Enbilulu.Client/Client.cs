6064fcec83526a4b3cb197f135c68891644663b4
gilmae <avocadia@fastmail.fm>
2023-04-15 11:32:04 +1000

    Adds a wait parameter when retrieving points from a stream

--- Enbilulu.Client/Client.cs
+++ Enbilulu.Client/Client.cs
@@ -2,83 +2,74 @@
 using System.Collections.Generic;
 using RestSharp;
 
-
 namespace Enbilulu;
 
 using Models;
 
-public class Client
-{
+public class Client {
     public Uri Endpoint { get; set; }
 
-    public Client()
-    {
+    public Client () { }
+
+    public Client (string host, int port = 16016) {
+        Endpoint = new Uri ($"{host}:{port}");
     }
 
-    public Client(string host, int port = 16016)
-    {
-        Endpoint = new Uri($"{host}:{port}");
-    }
+    public Stream CreateStream (string stream) {
+        var client = new RestClient (Endpoint);
 
-    public Stream CreateStream(string stream)
-    {
-        var client = new RestClient(Endpoint);
+        var request = new RestRequest ("/streams/{stream}", Method.Post);
+        request.AddUrlSegment ("stream", stream);
 
-        var request = new RestRequest("/streams/{stream}", Method.Post);
-        request.AddUrlSegment("stream", stream);
-
-        var response = client.Post<Stream>(request);
+        var response = client.Post<Stream> (request);
 
         return response;
     }
 
-    public Stream PutRecord(string stream, dynamic data)
-    {
-        var client = new RestClient(Endpoint);
+    public Stream PutRecord (string stream, dynamic data) {
+        var client = new RestClient (Endpoint);
 
-        var request = new RestRequest("/streams/{stream}/point", Method.Post);
-        request.AddUrlSegment("stream", stream);
-        request.AddJsonBody<object>(data as object);
+        var request = new RestRequest ("/streams/{stream}/point", Method.Post);
+        request.AddUrlSegment ("stream", stream);
+        request.AddJsonBody<object> (data as object);
 
-        var response = client.Post<Stream>(request);
-
-        return response;
-    }
-
-    public Stream GetStream(string stream)
-    {
-        var client = new RestClient(Endpoint);
-
-        var request = new RestRequest("/streams/{stream}", Method.Get);
-        request.AddUrlSegment("stream", stream);
-
-        var response = client.Get<Stream>(request);
-
-        return response;
-        
-    }
-
-    public Section GetRecordsFrom(string stream, int start, int limit)
-    {
-        var client = new RestClient(Endpoint);
-
-        var request = new RestRequest("/streams/{stream}/{start}/{limit}", Method.Get);
-        request.AddUrlSegment("stream", stream);
-        request.AddUrlSegment("start", start);
-        request.AddUrlSegment("limit", limit);
-
-        var response = client.Get<Section>(request);
+        var response = client.Post<Stream> (request);
 
         return response;
     }
 
-    public IList<string> ListStreams()
-    {
-        var client = new RestClient(Endpoint);
+    public Stream GetStream (string stream) {
+        var client = new RestClient (Endpoint);
 
-        var request = new RestRequest("/streams", Method.Get);
+        var request = new RestRequest ("/streams/{stream}", Method.Get);
+        request.AddUrlSegment ("stream", stream);
 
-        var response = client.Get<List<string>>(request);
+        var response = client.Get<Stream> (request);
+
+        return response;
+
+    }
+
+    public Section GetRecordsFrom (string stream, int start, int limit, int wait = 0) {
+        var client = new RestClient (Endpoint);
+
+        var request = new RestRequest ("/streams/{stream}/{start}/{limit}", Method.Get);
+        request.AddUrlSegment ("stream", stream);
+        request.AddUrlSegment ("start", start);
+        request.AddUrlSegment ("limit", limit);
+        request.AddQueryParameter ("wait", wait);
+
+        var response = client.Get<Section> (request);
+
+        return response;
+    }
+
+    public IList<string> ListStreams () {
+        var client = new RestClient (Endpoint);
+
+        var request = new RestRequest ("/streams", Method.Get);
+
+        var response = client.Get<List<string>> (request);
 
         return response;
     }
--- Enbilulu.Server/API/StreamHandler.cs
+++ Enbilulu.Server/API/StreamHandler.cs
@@ -1,82 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using libEnbilulu;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using System.IO;
 
-namespace EnbiluluServer
-{
-    [Route("/streams")]
+namespace EnbiluluServer {
+    [Route ("/streams")]
     [ApiController]
-    public class StreamsHandler : ControllerBase
-    {
+    public class StreamsHandler : ControllerBase {
         private Enbilulu _engine;
-        public StreamsHandler(Enbilulu engine)
-        {
+        public StreamsHandler (Enbilulu engine) {
             _engine = engine;
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetStreams() {
+        public async Task<ActionResult> GetStreams () {
 
-            var streams = await _engine.ListStreams();
+            var streams = await _engine.ListStreams ();
 
-            return Ok(streams);
+            return Ok (streams);
         }
 
         [HttpGet]
-        [Route("{name}")]
-        public async Task<ActionResult> GetStream(string name)
-        {
-            var stream = await _engine.GetStream(name);
-            if (stream == null)
-            {
-                return NotFound();
+        [Route ("{name}")]
+        public async Task<ActionResult> GetStream (string name) {
+            var stream = await _engine.GetStream (name);
+            if (stream == null) {
+                return NotFound ();
             }
-            return Ok(stream);
+            return Ok (stream);
         }
 
         [HttpGet]
-        [Route("{name}/{point}/{limit}")]
-        public async Task<ActionResult> GetRecords(string name, int point, int limit)
-        {
-            var data = await _engine.GetRecords(name, point, limit);
-            return Ok(data);
+        [Route ("{name}/{point}/{limit}")]
+        public async Task<ActionResult> GetRecords (string name, int point, int limit, [FromQuery] int wait = 0) {
+
+            Stopwatch stopwatch = new Stopwatch ();
+            stopwatch.Start ();
+            var data = await _engine.GetRecords (name, point, limit);
+
+            while (new List<object> (data.Records).Count < 1 && stopwatch.ElapsedMilliseconds < wait * 1000) {
+                data = await _engine.GetRecords (name, point, limit);
+            }
+            return Ok (data);
         }
 
         [HttpPost]
-        [Route("{name}")]
-        public async Task<ActionResult> CreateStream([FromRoute] string name)
-        {
-            var stream = await _engine.CreateStream(name);
-            return Ok(stream);
+        [Route ("{name}")]
+        public async Task<ActionResult> CreateStream ([FromRoute] string name) {
+            var stream = await _engine.CreateStream (name);
+            return Ok (stream);
         }
 
         [HttpPost]
-        [Route("{name}/point")]
-        public async Task<ActionResult> PutRecord(string name)
-        {
+        [Route ("{name}/point")]
+        public async Task<ActionResult> PutRecord (string name) {
             string data = "";
 
-            using (StreamReader reader = new StreamReader(this.Request.Body))
-            {
-                data = await reader.ReadToEndAsync();
+            using (StreamReader reader = new StreamReader (this.Request.Body)) {
+                data = await reader.ReadToEndAsync ();
             }
 
-            try
-            {
-                var point = await _engine.PutRecord(name, data);
-            }
-            catch (ArgumentException)
-            {
-                return NotFound("Stream Not Found");
+            try {
+                var point = await _engine.PutRecord (name, data);
+            } catch (ArgumentException) {
+                return NotFound ("Stream Not Found");
             }
 
-            var stream = await _engine.GetStream(name);
+            var stream = await _engine.GetStream (name);
 
-            return Ok(stream);
+            return Ok (stream);
         }
-
 
     }
 }
